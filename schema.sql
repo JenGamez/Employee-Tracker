81bc1959285b5d104ecf7772feae24a139c2887c
Jennifer Gamez <jenggamez@gmail.com>
2019-12-13 18:41:59 -0800

    added insert values and the department_id foreign key

--- schema.sql
+++ schema.sql
@@ -4,6 +4,8 @@ CREATE DATABASE employee_tracker;
 
 USE employee_tracker;
 
+DROP TABLE department;
+
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30),
@@ -14,15 +16,17 @@ CREATE TABLE employee_role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL(10,4),
-    department_id INT NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id),
     PRIMARY KEY (id)
 );
+
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES employee_role.id,
+    FOREIGN KEY (manager_id) REFERENCES employee.id INT NULL,
     PRIMARY KEY (id)
 );
--- seeds.sql
+++ seeds.sql
@@ -1,9 +1,36 @@
--- Seeds.sql file: "insert" statements to bootstrap fields with data...prepopulastes my database
+-- INSERT DATA FOR TABLES
 
 USE employee_tracker;
 
+-- DEPARTMENT DATA
 INSERT INTO department (name)
-VALUES ("Emerson"), ("Northeastern"), ("MIT");
+VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
+
+-- EMPLOYEE ROLE DATA
+INSERT INTO employee_role (title) 
+VALUES ("Sales Lead"), ("Salesperson"), ("Lead Engineer"), ("Software Engineer"), ("Account Manager"), ("Accountant"), ("Legal Team Lead");
+
+INSERT INTO employee_role (salary)
+VALUES (100,000), (80,000), (150,000), (120,000), (125,000), (250,000), (190,000);
+
+-- THIS SHOULD BE A FOREIGN KEY, I THINK, BUT HOW TO DO THIS?
+INSERT INTO employee_role (department_id)
+VALUES ()
+
+-- EMPLOYEE DATA
+INSERT INTO employee (first name)
+VALUES ("John"), ("Mike"), ("Ashley"), ("Kevin"), ("Malia"), ("Sarah"), ("Tom"), ("Christian");
+
+INSERT INTO employee (last name)
+VALUES ("Doe"), ("Chan"), ("Rodriguez"), ("Tupik"), ("Brown"), ("Lourd"), ("Alen"), ("Eckenrode");
+
+-- THIS SHOULD BE A FOREIGN KEY, I THINK, BUT HOW TO DO THIS?
+INSERT INTO employee (role_id)
+VALUES ()
+
+-- THIS SHOULD BE A FOREIGN KEY, I THINK, BUT HOW TO DO THIS?
+INSERT INTO employee (manager_id)
+VALUES ()
 
 
 
